f8a215c14ef0cfa7f97bc45e75e7a6a7b08a64e7
Oksana Yakimova <gagarinastreet46@mail.ru>
2023-04-09 15:19:03 +0500

    change initial commit

---
+++ src/main/java/ru/netology/PosterManager.java
@@ -0,0 +1,36 @@
+package ru.netology;
+public class PosterManager {
+    private String[] films = new String[0];
+    private int limit;
+    public PosterManager () {
+        this.limit = 10;
+    }
+    public PosterManager (int limit) {
+        this.limit = limit;
+    }
+    public void add(String film) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+    public String[] findAll() {
+        return films;
+    }
+
+    public String[] findLast() {
+        int resultLenght;
+        if (films.length < limit) {
+            resultLenght = films.length;
+        } else {
+            resultLenght = limit;
+        }
+        String[] tmp = new String[resultLenght];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
+}
